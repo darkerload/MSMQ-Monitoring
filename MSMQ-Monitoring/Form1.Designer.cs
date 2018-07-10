8fc9cb1bea9f0f98143a97ef51ec08e8efbaabad
darkerload <abdullahkaratekin@gmail.com>
2018-07-10 14:57:53 +0300

    Initial commit

---
+++ MSMQ-Monitoring/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace MSMQ_Monitoring
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnList = new System.Windows.Forms.Button();
+            this.lstMessage = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtMessagePath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtLabel = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblMessageId = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.lblMessageLabel = new System.Windows.Forms.Label();
+            this.txtMessageRead = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Path :";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(54, 9);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(138, 20);
+            this.txtPath.TabIndex = 1;
+            this.txtPath.Text = ".\\Private$\\TestPrivate";
+            // 
+            // btnList
+            // 
+            this.btnList.Location = new System.Drawing.Point(210, 9);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(118, 23);
+            this.btnList.TabIndex = 2;
+            this.btnList.Text = "Get All Messages";
+            this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // lstMessage
+            // 
+            this.lstMessage.FormattingEnabled = true;
+            this.lstMessage.Location = new System.Drawing.Point(15, 39);
+            this.lstMessage.Name = "lstMessage";
+            this.lstMessage.Size = new System.Drawing.Size(313, 303);
+            this.lstMessage.TabIndex = 3;
+            this.lstMessage.SelectedIndexChanged += new System.EventHandler(this.lstMessage_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSend);
+            this.groupBox1.Controls.Add(this.txtLabel);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtMessage);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtMessagePath);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(334, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(551, 157);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Message Creator";
+            // 
+            // txtMessagePath
+            // 
+            this.txtMessagePath.Location = new System.Drawing.Point(54, 30);
+            this.txtMessagePath.Name = "txtMessagePath";
+            this.txtMessagePath.Size = new System.Drawing.Size(138, 20);
+            this.txtMessagePath.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Path :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Message :";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(78, 60);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(236, 75);
+            this.txtMessage.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(359, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Label :";
+            // 
+            // txtLabel
+            // 
+            this.txtLabel.Location = new System.Drawing.Point(404, 33);
+            this.txtLabel.Multiline = true;
+            this.txtLabel.Name = "txtLabel";
+            this.txtLabel.Size = new System.Drawing.Size(130, 58);
+            this.txtLabel.TabIndex = 8;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(404, 111);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(130, 23);
+            this.btnSend.TabIndex = 9;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtMessageRead);
+            this.groupBox2.Controls.Add(this.lblMessageLabel);
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.lblMessageId);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Location = new System.Drawing.Point(334, 173);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(551, 169);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Selected Item Detail :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(338, 26);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 13);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Message :";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 26);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(70, 13);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "Message ID :";
+            // 
+            // lblMessageId
+            // 
+            this.lblMessageId.AutoSize = true;
+            this.lblMessageId.Location = new System.Drawing.Point(88, 26);
+            this.lblMessageId.Name = "lblMessageId";
+            this.lblMessageId.Size = new System.Drawing.Size(0, 13);
+            this.lblMessageId.TabIndex = 5;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(12, 60);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(39, 13);
+            this.label11.TabIndex = 6;
+            this.label11.Text = "Label :";
+            // 
+            // lblMessageLabel
+            // 
+            this.lblMessageLabel.AutoSize = true;
+            this.lblMessageLabel.Location = new System.Drawing.Point(88, 60);
+            this.lblMessageLabel.Name = "lblMessageLabel";
+            this.lblMessageLabel.Size = new System.Drawing.Size(0, 13);
+            this.lblMessageLabel.TabIndex = 7;
+            // 
+            // txtMessageRead
+            // 
+            this.txtMessageRead.Enabled = false;
+            this.txtMessageRead.Location = new System.Drawing.Point(341, 49);
+            this.txtMessageRead.Multiline = true;
+            this.txtMessageRead.Name = "txtMessageRead";
+            this.txtMessageRead.Size = new System.Drawing.Size(193, 101);
+            this.txtMessageRead.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 362);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lstMessage);
+            this.Controls.Add(this.btnList);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "MSMQ-Monitoring";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.ListBox lstMessage;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtMessagePath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblMessageLabel;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label lblMessageId;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtMessageRead;
+    }
+}
+
---
+++ MSMQ-Monitoring/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Windows.Forms;
+
+namespace MSMQ_Monitoring
+{
+    public partial class Form1 : Form
+    {
+        List<System.Messaging.Message> messageList = new List<System.Messaging.Message>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            string path = txtPath.Text;
+            messageList.Clear();
+            using (MessageQueue messageQueue = new MessageQueue(path))
+                messageList.AddRange(messageQueue.GetAllMessages());
+            FillList();
+        }
+
+        void FillList()
+        {
+            lstMessage.Items.Clear();
+            foreach (var message in messageList)
+                lstMessage.Items.Add(message.Id);
+        }
+
+        private void lstMessage_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string messageId = lstMessage.GetItemText(lstMessage.SelectedItem);
+            var getMessage = messageList.FirstOrDefault(c => c.Id == messageId);
+            getMessage.Formatter = new XmlMessageFormatter(new String[] { "System.String, mscorlib" });
+            txtMessageRead.Text = getMessage.Body.ToString();
+            lblMessageId.Text = getMessage.Id;
+            lblMessageLabel.Text = getMessage.Label;
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string path = txtMessagePath.Text;
+            bool isPathExist = MessageQueue.Exists(path);
+            if (!isPathExist)
+                MessageQueue.Create(path);
+            CreateMessage();
+        }
+
+        void CreateMessage()
+        {
+            var queue = new MessageQueue(txtPath.Text);
+            queue.Send(txtMessage.Text, txtLabel.Text);
+        }
+    }
+}
